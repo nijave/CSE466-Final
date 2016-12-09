835a9f0250dc65ce2f13bc2b9b4b49d91a8f4bab
Nick Venenga <nijave@gmail.com>
2016-12-09 15:40:39 -0500

    Updated SQL queries during ui implementation

--- Queries.sql
+++ Queries.sql
@@ -5,11 +5,11 @@ SELECT 'Total genes in common' as 'Description';
 SELECT COUNT(*) as 'Genes in Common' FROM (SELECT name FROM genes GROUP BY name HAVING COUNT(DISTINCT filename) > 1) t;
 
 SELECT 'Genes unique to each organism' as 'Description';
-SELECT genome_build as 'Organism', COUNT(*) as 'Count' 
-FROM genes JOIN genome USING(filename) 
-WHERE name IN (
-	SELECT name FROM genes GROUP BY name HAVING COUNT(DISTINCT filename) = 1
-) GROUP BY genome_build;
+SELECT Organism, SUM(Count) as 'Count' FROM (
+	SELECT genome_build as 'Organism', 1 as 'Count' 
+	FROM genes JOIN genome USING(filename) 
+	GROUP BY name HAVING COUNT(DISTINCT genome_build) = 1
+) t GROUP BY Organism ORDER BY Organism ASC
 
 SELECT 'Gene count for each organism' as 'Description';
 SELECT genome_build as 'Organism', COUNT(DISTINCT name) as 'Gene Count' FROM genes LEFT JOIN genome ON genome.filename = genes.filename GROUP BY genome_build;
@@ -19,7 +19,7 @@ SELECT genome_build as 'Organism', 'RNA' as 'Type', COUNT(*) as 'Count' FROM rna
 UNION ALL
 SELECT genome_build as 'Organism', 'Gene' as 'Type', COUNT(*) as 'Count' FROM genes JOIN genome USING(filename) GROUP BY genome_build
 UNION ALL
-SELECT genome_build as 'Organism', 'Transcripts' as 'Type', COUNT(*) as 'Count' FROM transcripts JOIN genome USING(filename) GROUP BY genome_build;
+SELECT genome_build as 'Organism', 'Transcripts' as 'Type', COUNT(*) as 'Count' FROM transcripts JOIN genome USING(filename) GROUP BY genome_build ORDER BY Organism;
 
 SELECT 'Gene (bio) type totals' as 'Description';
-SELECT genome_build as 'Organism', `type` as 'Type', COUNT(`type`) as 'Count' FROM genes JOIN genome USING(filename) GROUP BY filename, `type` ORDER BY filename, `type`;
+SELECT genome_build as 'Organism', `type` as 'Type', COUNT(`type`) as 'Count' FROM genes JOIN genome USING(filename) GROUP BY filename, `type` ORDER BY Organism ASC, Count DESC;
